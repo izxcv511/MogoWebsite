86697932c571d9e270a31ead0f8d20087d9f255e
izxcv511 <izxcv511@gmail.com>
2021-06-07 12:30:28 +0700

    fix shadow

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,5 +1,6 @@
 const headerToggle = document.querySelector(".header__toggle");
 const headerMenu = document.querySelector(".header-menu");
+const headerShadow = document.querySelector(".header-menu");
 
 const expandClass = "is-expand";
 
@@ -7,7 +8,7 @@ headerToggle.addEventListener("click", function () {
   headerMenu.classList.add(expandClass);
 });
 
-document.querySelector(body).addEventListener("click", function (e) {
+document.querySelector("body").addEventListener("click", function (e) {
   console.log(e.target);
   if (e.target.matches(".shadow")) {
     headerMenu.classList.remove(expandClass);
